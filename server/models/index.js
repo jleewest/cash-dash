9da283db061627ff2546cc2d738524864571af7a
Goran Levanic <glevanic@gmail.com>
2024-01-11 10:47:30 +0100

    feat: connect to sequelize database

---
+++ server/models/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const { Sequelize } = require('./sequelize');
+
+const sequelize = new Sequelize('cash_dash', 'goran', '', {
+  host: 'localhost',
+  dialect: 'postgres',
+});
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
+
+module.exports = sequelize;
