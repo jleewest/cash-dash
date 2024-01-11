25ee35d82701939790355409c3d011bd79524870
Goran Levanic <glevanic@gmail.com>
2024-01-11 20:39:20 +0100

    feat: fetch data from db

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+import NavBar from './components/NavBar.jsx';
+import Transactions from './components/Transactions.jsx';
+import Header from './components/Header.jsx';
 
 function App() {
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/transactions')
+      .then((res) => res.json())
+      .then((data) => setTransactions(data));
+  }, []);
+
   return (
-    <div>
-      <h1>Hello</h1>
+    <div id="app">
+      <Header />
+      <NavBar />
+      <Transactions transactions={transactions} />
     </div>
   );
 }
