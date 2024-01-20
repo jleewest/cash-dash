68f892cdb18c969804a6605082edb8637129e836
Jenna Westendorf <jenna.westendorf@gmail.com>
2024-01-19 17:45:59 -0800

    refactor: main to ts

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,11 @@ import { ChakraProvider } from '@chakra-ui/react';
 import App from './App.tsx';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <ChakraProvider>
       <App />
