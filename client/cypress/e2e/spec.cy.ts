78ccc86c3f3b76fd06260fe45d099574db4dd322
Jenna Westendorf <jenna.westendorf@gmail.com>
2024-01-24 16:58:11 -0800

    test: can edit current list of transactions

--- client/cypress/e2e/spec.cy.ts
+++ client/cypress/e2e/spec.cy.ts
@@ -12,4 +12,17 @@ describe('CashDash', () => {
     cy.contains('Save Transaction').click();
     cy.contains('Not more bills!').should('be.visible');
   });
+  it('should edit transaction in current list of transactions', () => {
+    cy.visit('http://localhost:5173');
+    cy.contains('Transactions').click();
+    cy.contains('Freelancing').siblings().find('button').eq(0).click();
+    cy.get('[data-testid=category-options]').select('Salary');
+    cy.get('[data-testid=category-options]').should('have.value', 'Salary');
+    cy.get('[data-testid=setAmount]').clear().type('300');
+    cy.get('[data-testid=setAmount]').should('have.value', '300');
+    cy.get('[data-testid=setNote]').clear().type('More money!');
+    cy.get('[data-testid=setNote]').should('have.value', 'More money!');
+    cy.contains('Save Transaction').click();
+    cy.contains('More money!').should('be.visible');
+  });
 });
