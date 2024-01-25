dbd682adf0bc8be7bb7844a4600fe77941a23238
Jenna Westendorf <jenna.westendorf@gmail.com>
2024-01-24 17:21:25 -0800

    test: delete transaction needs more work

--- client/cypress/e2e/spec.cy.ts
+++ client/cypress/e2e/spec.cy.ts
@@ -3,8 +3,11 @@ describe('CashDash', () => {
     cy.visit('http://localhost:5173');
     cy.contains('Transactions').click();
     cy.contains('Add Transaction').click();
-    cy.get('[data-testid=category-options]').select('Bills');
-    cy.get('[data-testid=category-options]').should('have.value', 'Bills');
+    cy.get('[data-testid=category-options]').select('Transportation');
+    cy.get('[data-testid=category-options]').should(
+      'have.value',
+      'Transportation'
+    );
     cy.get('[data-testid=setAmount]').type('300');
     cy.get('[data-testid=setAmount]').should('have.value', '300');
     cy.get('[data-testid=setNote]').type('Not more bills!');
@@ -15,14 +18,22 @@ describe('CashDash', () => {
   it('should edit transaction in current list of transactions', () => {
     cy.visit('http://localhost:5173');
     cy.contains('Transactions').click();
-    cy.contains('Freelancing').siblings().find('button').eq(0).click();
-    cy.get('[data-testid=category-options]').select('Salary');
-    cy.get('[data-testid=category-options]').should('have.value', 'Salary');
-    cy.get('[data-testid=setAmount]').clear().type('300');
-    cy.get('[data-testid=setAmount]').should('have.value', '300');
-    cy.get('[data-testid=setNote]').clear().type('More money!');
-    cy.get('[data-testid=setNote]').should('have.value', 'More money!');
+    cy.contains('Transportation').siblings().find('button').eq(0).click();
+    cy.get('[data-testid=category-options]').select('Subscriptions');
+    cy.get('[data-testid=category-options]').should(
+      'have.value',
+      'Subscriptions'
+    );
+    cy.get('[data-testid=setAmount]').clear().type('3000');
+    cy.get('[data-testid=setAmount]').should('have.value', '3000');
+    cy.get('[data-testid=setNote]').clear().type('So much money!');
+    cy.get('[data-testid=setNote]').should('have.value', 'So much money!');
     cy.contains('Save Transaction').click();
-    cy.contains('More money!').should('be.visible');
+    cy.contains('So much money!').should('be.visible');
+  });
+  it('should delete transaction in current list of transactions', () => {
+    cy.visit('http://localhost:5173');
+    cy.contains('Transactions').click();
+    cy.contains('Subscriptions').siblings().find('button').eq(1).click();
   });
 });
